f6e09d46b87747a0492af325be2338e836cc46d6
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-03-13 11:57:47 -0700

    исправлен критический баг в классе авторизации и регистрации

--- ServerApplication/RegistrationAndLogin.cs
+++ ServerApplication/RegistrationAndLogin.cs
@@ -11,25 +11,21 @@ namespace RegistrationAndLogin
         public static bool RegistrName(string Login)
         {
             RegistrationName = TrakerBD.GetName();
-            bool Result = false;
             foreach (string name in RegistrationName)
             {
                 if (name == Login)
                 {
 
-                    Result = false;
+                    return false;
 
                 }
-                else
-                {
-                    Result = true;
-                }
+
             }
-            return Result;
+            return true;
         }
         public static bool RegistrPassword(string password)
         {
-            if (password.Length >= 8 && password.Length <= 14)
+            if (password.Length >= 8)
             {
                 return true;
             }
@@ -41,40 +37,30 @@ namespace RegistrationAndLogin
         public static bool LoginName(string Login)
         {
             LoginNames = TrakerBD.GetName();
-            bool Result = false;
             foreach (string name in LoginNames)
             {
                 if (name == Login)
                 {
 
-                    Result = true;
+                    return true;
 
                 }
-                else
-                {
-                    Result = false;
-                }
             }
-            return Result;
+            return false;
         }
         public static bool LoginPassword(string password)
         {
             LoginPass = TrakerBD.GetPassword();
-            bool Result = false;
             foreach (string pass in LoginPass)
             {
                 if (pass == password)
                 {
 
-                    Result = true;
+                    return true;
 
                 }
-                else
-                {
-                    Result = false;
-                }
             }
-            return Result;
+            return false;
         }
     }
 }
